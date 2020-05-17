d101f2193696e807405620b94ed70c41d9b43b4f
Gabriel <rdeckard.gt@gmail.com>
2020-05-17 10:57:08 +0200

    `bin/rails generate administrate:install` + code style

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   root controller: :posts, action: :index
 
   resources :posts, only: %i[index create]
+
+  namespace :admin do
+    resources :posts
+
+    root controller: :posts, action: :index
+  end
 end
