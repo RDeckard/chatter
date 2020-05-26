79c69e05b75ff31bba45ac51628efcb1b3ee95b1
Gabriel <rdeckard.gt@gmail.com>
2020-05-26 21:00:56 +0200

    Example of raw ActionCable usage inside a Phaser App

--- app/javascript/channels/timeline_channel.js
+++ app/javascript/channels/timeline_channel.js
@@ -1,7 +1,7 @@
 import CableReady from 'cable_ready'
 import consumer from "./consumer"
 
-consumer.subscriptions.create("TimelineChannel", {
+export default consumer.subscriptions.create("TimelineChannel", {
   received(data) {
     if (data.cableReady) CableReady.perform(data.operations)
   }
